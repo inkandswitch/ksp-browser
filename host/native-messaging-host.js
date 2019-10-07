61dee0c529fbafad0d13f97f8216b9cecd9556c7
Peter van Hardenberg <pvh@pvh.ca>
2019-10-07 16:59:12 -0700

    works. wow.

--- host/native-messaging-host.js
+++ host/native-messaging-host.js
@@ -1,14 +1,24 @@
 #!node
 
-// Might be good to use an explicit path to node on the shebang line
-// in case it isn't in PATH when launched by Chrome
-
+const ipc = require('node-ipc')
 var sendMessage = require('native-messaging')(handleMessage)
+
+// shamelessly lifted from pushpin core
+const USER = process.env.NAME || process.env.USER || process.env.USERNAME
+ipc.config.silent = true
+ipc.config.appspace = `pushpin.${USER}.`
+ipc.config.maxRetries = 2
+ipc.config.maxConnections = 1
+ipc.config.id = 'clipper'
 
 // pvh's extension ID is: fcanljfkfdhddpeikfmdojfclefaeioj
 
 function handleMessage(req) {
-  if (req.message === 'ping') {
-    sendMessage({ message: 'pong', body: 'hello from nodejs app' })
-  } else sendMessage({ message: 'unrecognized', body: 'no clue what to do' })
+  ipc.connectTo('renderer', () => {
+    ipc.of.renderer.on('connect', function() {
+      ipc.of.renderer.emit('message', req)
+      ipc.disconnect('renderer')
+      sendMessage({ message: 'forwarded to pushpin' })
+    })
+  })
 }
